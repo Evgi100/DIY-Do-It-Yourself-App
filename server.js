87b0e36b8b3016eb2cfd7cf72435e784acf2154c
Evgi100 <evgeni.shapira@gmail.com>
2018-02-28 18:53:42 +0200

    prepare for heroku deploy

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const path = require('path');
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.all('*', function(req, res) {
+  res.status(200).sendFile(__dirname+'/dist/index.html')
 });
 
 
